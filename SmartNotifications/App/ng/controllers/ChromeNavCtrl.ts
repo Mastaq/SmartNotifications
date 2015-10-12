0a954071dbedc9643f2d21fa4442cc6841ec3727
sergei_sergeev <sergeev.srg@gmail.com>
2015-10-13 00:36:50 +0300

    Chrome nav fixes.

--- SmartNotifications/App/ng/controllers/ChromeNavCtrl.ts
+++ SmartNotifications/App/ng/controllers/ChromeNavCtrl.ts
@@ -8,11 +8,17 @@ namespace SN {
             "$window"
         ];
 
-		info: string;
+		hostUrl: string;
+		hostTitle: string;
+		appUrl: string;
 
         constructor(private $scope: ICtrlScope<ChromeNavCtrl>, private $http: ng.IHttpService, private $window: ng.IWindowService) {
 
             $scope.vm = this;
+			this.hostUrl = "http://host.url";
+			this.hostTitle = "COOL";
+	        this.appUrl = "http://app.url";
+
         }
     }
 
