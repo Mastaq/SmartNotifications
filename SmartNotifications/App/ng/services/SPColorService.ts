7e9d5ac492556525017cc83da028bad346281749
sergei_sergeev <sergeev.srg@gmail.com>
2015-11-16 02:31:25 +0300

    UI tweaks

--- SmartNotifications/App/ng/services/SPColorService.ts
+++ SmartNotifications/App/ng/services/SPColorService.ts
@@ -14,6 +14,10 @@ namespace SN {
 			this.$ = vendorsFactory.$;
 		}
 
+		private getElementColor(selector: string): string {
+			return this.$(selector).css("color");
+		}
+
 	    private getElementBackground(selector:string): string {
 			return this.$(selector).css("background-color");
 	    }
@@ -24,7 +28,14 @@ namespace SN {
 
 		public applyBackgrounds() {
 			var background = this.getElementBackground(".ms-rteTable-1 tr.ms-rteTableHeaderRow-1");
-			this.$("head").append(String.format("<style type='text/css'>#sn-view {{background-color:{0};}} </style>", background));
+			var linksColor = this.getElementColor(".ms-core-listMenu-item");
+			var linksBackground = this.getElementBackground(".ms-HoverBackground-bgColor");
+			var activeLinkBackground = this.getElementBackground(".ms-core-listMenu-selected");
+			this.$("head").append(String.format("<style type='text/css'>" +
+				".navigation-menu ul a {{color: {1};}} " +
+				".navigation-menu ul a:hover {{background-color:{2};}} " +
+				".navigation-menu ul li.active a {{background-color: {3};color: {1};}}" +
+				"</style>", background, linksColor, linksBackground, activeLinkBackground));
 		}
     }
 
